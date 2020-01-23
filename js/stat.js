d4e8bca8f6382863f60cb9f16224d3c75ec6e492
Maxim <m.radik25@gmail.com>
2020-01-23 21:37:18 +0300

    update stat screen

--- js/stat.js
+++ js/stat.js
@@ -4,7 +4,9 @@ var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
 var CLOUD_X = 100;
 var CLOUD_Y = 10;
+var cloudColor = '#ffffff';
 var SHADOW_GAP = 10;
+var shadowColor = 'rgba(0, 0, 0, 0.7)';
 
 var FULL_BAR_HEIGHT = 150;
 var BAR_WIDTH = 40;
@@ -13,8 +15,10 @@ var BAR_GAP = 50;
 var barX = CLOUD_X + BAR_GAP;
 var barY = 230;
 var maxBarWidth = FULL_BAR_HEIGHT - BAR_GAP;
+var userBarColor = 'rgba(255, 0, 0, 1)';
 
 var TEXT_GAP = 5;
+var textColor = '#000000';
 
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
@@ -36,9 +40,9 @@ var getBlueColor = function () {
 };
 
 window.renderStatistics = function (ctx, names, times) {
-  renderCloud(ctx, CLOUD_X + SHADOW_GAP, CLOUD_Y + SHADOW_GAP, 'rgba(0, 0, 0, 0.7)');
-  renderCloud(ctx, CLOUD_X, CLOUD_Y, '#ffffff');
-  ctx.fillStyle = '#000000';
+  renderCloud(ctx, CLOUD_X + SHADOW_GAP, CLOUD_Y + SHADOW_GAP, shadowColor);
+  renderCloud(ctx, CLOUD_X, CLOUD_Y, cloudColor);
+  ctx.fillStyle = textColor;
   ctx.font = '16px PT Mono';
   ctx.fillText('Ура вы победили!', CLOUD_X + (BAR_GAP >> 1), CLOUD_Y + (BAR_GAP >> 1) + (TEXT_GAP << 1));
   ctx.fillText('Список результатов:', CLOUD_X + (BAR_GAP >> 1), CLOUD_Y + BAR_GAP + (TEXT_GAP << 1));
@@ -46,15 +50,15 @@ window.renderStatistics = function (ctx, names, times) {
   var maxTime = getMaxElement(times);
 
   for (var i = 0; i < names.length; i++) {
-    ctx.fillStyle = '#000000';
+    ctx.fillStyle = textColor;
     ctx.fillText(Math.floor(times [i]), barX + (BAR_WIDTH + BAR_GAP) * i, barY - TEXT_GAP * 2 - maxBarWidth * times[i] / maxTime);
     if (names [i] === 'Вы') {
-      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+      ctx.fillStyle = userBarColor;
     } else {
       ctx.fillStyle = getBlueColor();
     }
     ctx.fillRect(barX + (BAR_WIDTH + BAR_GAP) * i, barY - (maxBarWidth * times[i] / maxTime), BAR_WIDTH, maxBarWidth * times[i] / maxTime);
-    ctx.fillStyle = '#000000';
+    ctx.fillStyle = textColor;
     ctx.fillText(names [i], barX + (BAR_WIDTH + BAR_GAP) * i, barY + BAR_GAP / 2);
   }
 };
