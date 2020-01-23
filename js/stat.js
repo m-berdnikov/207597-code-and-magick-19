920051d7733a89c1125b47e1917c4852f72409bf
Maxim <m.radik25@gmail.com>
2020-01-23 20:50:05 +0300

    update stat scrtipt

--- js/stat.js
+++ js/stat.js
@@ -4,21 +4,23 @@ var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
 var CLOUD_X = 100;
 var CLOUD_Y = 10;
-var cloudColor = '#ffffff';
+var CLOUD_COLOR = '#ffffff';
 var SHADOW_GAP = 10;
-var shadowColor = 'rgba(0, 0, 0, 0.7)';
+var SHADOW_COLOR = 'rgba(0, 0, 0, 0.7)';
 
 var FULL_BAR_HEIGHT = 150;
 var BAR_WIDTH = 40;
 var BAR_GAP = 50;
 
 var barX = CLOUD_X + BAR_GAP;
-var barY = 230;
+var BAR_Y = 230;
 var maxBarWidth = FULL_BAR_HEIGHT - BAR_GAP;
-var userBarColor = 'rgba(255, 0, 0, 1)';
+var USER_BAR_COLOR = 'rgba(255, 0, 0, 1)';
 
 var TEXT_GAP = 5;
-var textColor = '#000000';
+var TEXT_COLOR = '#000000';
+
+var textX = CLOUD_X + (BAR_GAP >> 1);
 
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
@@ -39,26 +41,34 @@ var getBlueColor = function () {
   return 'hsl(239, ' + Math.floor(Math.random() * 100) + '%, 50%)';
 };
 
+var getRandomBar = function (ctx, x, y) {
+  ctx.fillRect(x, BAR_Y - y, BAR_WIDTH, y);
+};
+
 window.renderStatistics = function (ctx, names, times) {
-  renderCloud(ctx, CLOUD_X + SHADOW_GAP, CLOUD_Y + SHADOW_GAP, shadowColor);
-  renderCloud(ctx, CLOUD_X, CLOUD_Y, cloudColor);
-  ctx.fillStyle = textColor;
+  renderCloud(ctx, CLOUD_X + SHADOW_GAP, CLOUD_Y + SHADOW_GAP, SHADOW_COLOR);
+  renderCloud(ctx, CLOUD_X, CLOUD_Y, CLOUD_COLOR);
+  ctx.fillStyle = TEXT_COLOR;
   ctx.font = '16px PT Mono';
-  ctx.fillText('Ура вы победили!', CLOUD_X + (BAR_GAP >> 1), CLOUD_Y + (BAR_GAP >> 1) + (TEXT_GAP << 1));
-  ctx.fillText('Список результатов:', CLOUD_X + (BAR_GAP >> 1), CLOUD_Y + BAR_GAP + (TEXT_GAP << 1));
+  ctx.fillText('Ура вы победили!', textX, CLOUD_Y + (BAR_GAP >> 1) + (TEXT_GAP << 1));
+  ctx.fillText('Список результатов:', textX, CLOUD_Y + BAR_GAP + (TEXT_GAP << 1));
 
   var maxTime = getMaxElement(times);
 
   for (var i = 0; i < names.length; i++) {
-    ctx.fillStyle = textColor;
-    ctx.fillText(Math.floor(times [i]), barX + (BAR_WIDTH + BAR_GAP) * i, barY - TEXT_GAP * 2 - maxBarWidth * times[i] / maxTime);
+
+    var elemenX = barX + (BAR_WIDTH + BAR_GAP) * i;
+    var randomBarWidth = maxBarWidth * times[i] / maxTime;
+
+    ctx.fillStyle = TEXT_COLOR;
+    ctx.fillText(Math.floor(times [i]), elemenX, BAR_Y - TEXT_GAP * 2 - randomBarWidth);
     if (names [i] === 'Вы') {
-      ctx.fillStyle = userBarColor;
+      ctx.fillStyle = USER_BAR_COLOR;
     } else {
       ctx.fillStyle = getBlueColor();
     }
-    ctx.fillRect(barX + (BAR_WIDTH + BAR_GAP) * i, barY - (maxBarWidth * times[i] / maxTime), BAR_WIDTH, maxBarWidth * times[i] / maxTime);
-    ctx.fillStyle = textColor;
-    ctx.fillText(names [i], barX + (BAR_WIDTH + BAR_GAP) * i, barY + BAR_GAP / 2);
+    getRandomBar(ctx, elemenX, randomBarWidth);
+    ctx.fillStyle = TEXT_COLOR;
+    ctx.fillText(names [i], elemenX, BAR_Y + BAR_GAP / 2);
   }
 };
